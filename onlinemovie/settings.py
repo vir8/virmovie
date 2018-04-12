89c6e26c78f54fc192404f4c77053a8ed1c004cb
vir8 <37437817+vir8@users.noreply.github.com>
2018-04-12 23:13:51 +0530

    Update settings.py

--- onlinemovie/settings.py
+++ onlinemovie/settings.py
@@ -131,5 +131,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
