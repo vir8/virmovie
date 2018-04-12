0e71d195963d26e8fae6a50aa62d21af619b82c3
vir8 <37437817+vir8@users.noreply.github.com>
2018-04-12 22:57:13 +0530

    Update settings.py

--- onlinemovie/settings.py
+++ onlinemovie/settings.py
@@ -126,5 +126,8 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
